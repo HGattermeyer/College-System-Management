d08cb0f95647d067a5816fafccc9f175f07789e6
HGattermeyer <henrique1989@gmail.com>
2021-11-23 13:13:04 -0300

    Student and Subject Services

--- College System Management/College System Management/Services/StudentService.cs
+++ College System Management/College System Management/Services/StudentService.cs
@@ -1,6 +1,34 @@
-﻿namespace College_System_Management.Services
+﻿using College_System_Management.Data;
+using College_System_Management.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace College_System_Management.Services
 {
     public class StudentService
     {
+        private readonly College_System_ManagementContext _context;
+
+        public StudentService(College_System_ManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Student>> FindAllAsync()
+        {
+            return await _context.Student.ToListAsync();
+        }
+
+        public async Task<List<Student>> FindAllByCourseIdAsync(int id)
+        {
+            return await _context.Student.Where(obj => obj.CourseId == id).Include(g => g.Grades).ToListAsync();
+        }
+
+        public async Task<Student> FindByIdAsync(int id)
+        {
+            return await _context.Student.FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
---
+++ College System Management/College System Management/Services/SubjectService.cs
@@ -0,0 +1,40 @@
+﻿using College_System_Management.Data;
+using College_System_Management.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace College_System_Management.Services
+{
+    public class SubjectService
+    {
+        private readonly College_System_ManagementContext _context;
+
+        public SubjectService(College_System_ManagementContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Subject>> FindAllAsync()
+        {
+            return await _context.Subject.ToListAsync();
+        }
+
+        public async Task<Subject> FindByIdAsync(int id)
+        {
+            return await _context.Subject.FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        public async Task<List<Subject>> FindByCourseIdAsync(int id)
+        {
+            return await _context.Subject.Where(obj => obj.CourseId == id).ToListAsync();
+        }
+
+        public async Task<List<Subject>> FindAllByCourseIdAsync(int id)
+        {
+            return await _context.Subject.Where(obj => obj.CourseId == id).ToListAsync();
+        }
+
+    }
+}
